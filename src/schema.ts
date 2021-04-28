6f29692ddbc4d93cebd1eda7bd2999105a9edbb9
lturnbullcdw <l.turnbull@uk.cdw.com>
2021-04-28 15:04:50 +0100

    clean out repo to try again

--- src/schema.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLSchema } from "graphql";
-import { QueryType, MutationType } from "./graphql";
-
-const schema = new GraphQLSchema({
-    query: QueryType,
-    mutation: MutationType,
-});
-
-export default schema;
