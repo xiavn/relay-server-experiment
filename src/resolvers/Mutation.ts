977dd8cf101aeffd74ae05977590496af8895cd9
lturnbullcdw <l.turnbull@uk.cdw.com>
2021-05-21 16:44:22 +0100

    remove uneeded mutations

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -4,34 +4,6 @@ import { MutationResolvers } from 'src/generated/graphql';
 import { APP_SECRET, getUserId } from 'src/utils';
 
 const mutationResolvers: MutationResolvers = {
-    signup: async (parent, args, context, info) => {
-        const password = await bcrypt.hash(args.password, 10);
-        const user = await context.prisma.user.create({
-            data: { ...args, password },
-        });
-        const token = jwt.sign({ userId: user.id }, APP_SECRET);
-        return { token, user };
-    },
-    login: async (parent, args, context, info) => {
-        const user = await context.prisma.user.findUnique({
-            where: { email: args.email },
-        });
-        if (!user) {
-            throw new Error('No user found');
-        }
-
-        const valid = await bcrypt.compare(args.password, user.password);
-        if (!valid) {
-            throw new Error('Invalid password');
-        }
-
-        const token = jwt.sign({ userId: user.id }, APP_SECRET);
-
-        return {
-            token,
-            user,
-        };
-    },
     vote: async (parent, args, context, info) => {
         const { userId } = context;
         const vote = await context.prisma.vote.findUnique({
