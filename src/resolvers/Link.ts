b4822b0e0f6e0f682d29fa5bee0b9e3cc987b7da
lturnbullcdw <l.turnbull@uk.cdw.com>
2021-05-21 16:03:57 +0100

    remove posted by resolver

--- src/resolvers/Link.ts
+++ src/resolvers/Link.ts
@@ -1,11 +1,6 @@
 import { LinkResolvers } from 'src/generated/graphql';
 
 const linkResolvers: LinkResolvers = {
-    postedBy: async (parent, args, context) => {
-        return await context.prisma.link
-            .findUnique({ where: { id: parent.id } })
-            .postedBy();
-    },
     votes: async (parent, args, context) => {
         return await context.prisma.link
             .findUnique({ where: { id: parent.id } })
